27f91c5eb54365c29a9b056337fa003460921423
YoRHa <snowline2015@gmail.com>
2023-07-15 00:17:23 +0700

    Update Installation.py

--- Automation/Python/Installation.py
+++ Automation/Python/Installation.py
@@ -14,16 +14,14 @@ subprocess.run(
 # ElasticSearch
 subprocess.run(
     'helm repo add elastic https://helm.elastic.co && helm repo update && '
-    'helm install elasticsearch elastic/elasticsearch -f https://raw.githubusercontent.com/snowline2015/' 
-        'Kubernetes-Security/main/Doc/Installation/Helm/elasticsearch.yaml', 
-    shell=True)
+    'helm install elasticsearch elastic/elasticsearch -f '
+    'https://raw.githubusercontent.com/snowline2015/Kubernetes-Security/main/Doc/Installation/Helm/elasticsearch.yaml', shell=True)
 
 
 
 # Kibana
-subprocess.run('helm install kibana elastic/kibana -f https://raw.githubusercontent.com/snowline2015/' 
-                    'Kubernetes-Security/main/Doc/Installation/Helm/kibana.yaml', 
-               shell=True)
+subprocess.run('helm install kibana elastic/kibana -f '
+               'https://raw.githubusercontent.com/snowline2015/Kubernetes-Security/main/Doc/Installation/Helm/kibana.yaml', shell=True)
 
 
 
@@ -32,7 +30,7 @@ subprocess.run('helm install kibana elastic/kibana -f https://raw.githubusercont
 secret = subprocess.run("kubectl get secrets --namespace=default elasticsearch-master-credentials -ojsonpath='{.data.password}' | base64 -d", 
                         shell=True, capture_output=True, text=True).stdout
 ip = subprocess.run("kubectl get service --namespace=default elasticsearch-master -o jsonpath='{.spec.clusterIP}'", 
-                                  shell=True, capture_output=True, text=True).stdout
+                    shell=True, capture_output=True, text=True).stdout
 
 ## Edit
 manifest = requests.get('https://raw.githubusercontent.com/snowline2015/Kubernetes-Security/'
@@ -48,8 +46,14 @@ subprocess.run(f'kubectl apply -f {os.path.dirname(__file__)}/manifest.yaml', sh
 
 
 
+# Metrics Server
+subprocess.run('kubectl apply -f '
+               'https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml', shell=True)
+
+subprocess.run('kubectl patch -n kube-system deployment metrics-server --type=json -p \'[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--kubelet-insecure-tls"}]\'', shell=True)
+
+
+
 # Frontend & Backend
 subprocess.run('kubectl apply -f https://raw.githubusercontent.com/huyvdq2/tetragon_fe/main/k8s.yaml && '
-               'kubectl apply -f https://raw.githubusercontent.com/snowline2015/Kubernetes-Security/main/Doc/'
-                    'Security/backend.yaml', 
-               shell=True)
+               'kubectl apply -f https://raw.githubusercontent.com/snowline2015/Kubernetes-Security/main/Doc/Security/backend.yaml', shell=True)
